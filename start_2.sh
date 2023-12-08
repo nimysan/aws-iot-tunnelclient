38007b249cf72c83b401cf5ea4805049ceb78484
nimysan <234aini@gmail.com>
2023-12-08 15:44:18 +0800

    add second device:

---
+++ start_2.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#Filename start.sh
+mvn clean package
+mvn -X exec:java -Dexec.mainClass=top.cuteworld.iotdemo.SSHTunnelDaemon -Dexec.args='--endpoint a1cw44obfpxk2h-ats.iot.ap-southeast-1.amazonaws.com --client_id sdk-java --topic sdk/test/java --ca_file ./certs/root-CA.crt --cert ./lawn_mower_2.cert.pem --key ./lawn_mower_2.private.key --thing lawn_mower_2'
